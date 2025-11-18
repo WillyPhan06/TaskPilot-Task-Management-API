ce774f74a516f8f39ddb9617fb1431c84059e318
Willy Phan <resuviketer4@gmail.com>
2025-11-18 22:29:13 +0700

    Day 9

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     DESCRIPTION: str = "Task management API with FastAPI by WillyPhan"
     AUTHOR: str = "Willy Phan"
-    DATABASE_URL: str = "sqlite+aiosqlite:///./taskpilot.db"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
